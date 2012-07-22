ecea57a394a124b270f811701683d5be5163b8c2
seike takumi <tako......forest@gmail.com>
2012-07-22 22:46:59 +0900

    Initial Commit

---
+++ CvSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CvSample
+//
+//  Created by 巧 清家 on 12/07/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CvSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CvSample
+//
+//  Created by 巧 清家 on 12/07/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
